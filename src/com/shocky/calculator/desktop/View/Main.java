bf16c517ee5d17afbe2db4b15c2a29f349c18522
Bojan Ivanović <bojan.ivanovic6@gmail.com>
2016-10-04 22:10:13 +0200

    initial commit

---
+++ src/com/shocky/calculator/desktop/View/Main.java
@@ -0,0 +1,35 @@
+package com.shocky.calculator.desktop.View;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by bivanovic on 03/10/2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*JFrame mainWindow = new JFrame("Calculator");
+        Label emptyLabel = new Label();
+        JPanel btnPanel = new JPanel();
+        btnPanel.setLayout(null);
+        JButton button1 = new JButton("1");
+
+        btnPanel.setBounds(30, 30, 200, 150);
+        button1.setBounds(1, 1, 98, 48);
+        btnPanel.add(button1);
+        mainWindow.add(btnPanel);
+
+        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainWindow.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+
+
+        //mainWindow.pack();
+        mainWindow.setSize(300, 750);
+        mainWindow.setVisible(true);*/
+
+
+        Window mainWindow = new Window();
+    }
+}
---
+++ src/com/shocky/calculator/desktop/View/Window.java
@@ -0,0 +1,62 @@
+package com.shocky.calculator.desktop.View;
+
+import javax.swing.*;
+
+/**
+ * Created by bivanovic on 03/10/2016.
+ */
+public class Window {
+
+    private int windowWidth;
+    private int windowHeight;
+
+    private String windowTitle;
+
+    public int getWindowWidth() {
+        return windowWidth;
+    }
+
+    public void setWindowWidth(int windowWidth) {
+        this.windowWidth = windowWidth;
+    }
+
+    public int getWindowHeight() {
+        return windowHeight;
+    }
+
+    public void setWindowHeight(int windowHeight) {
+        this.windowHeight = windowHeight;
+    }
+
+    public String getWindowTitle() {
+        return windowTitle;
+    }
+
+    public void setWindowTitle(String windowTitle) {
+        this.windowTitle = windowTitle;
+    }
+
+    public Window(){
+        this.setWindowHeight(600);
+        this.setWindowWidth(450);
+        this.setWindowTitle("Calculator");
+
+        JFrame mainWindow = new JFrame(this.getWindowTitle());
+        mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainWindow.setSize(this.getWindowWidth(), this.getWindowHeight());
+        mainWindow.setVisible(true);
+    }
+
+    public Window(int windowHeight, int windowWidth, String windowTitle) {
+        this.setWindowWidth(windowWidth);
+        this.setWindowHeight(windowHeight);
+        this.setWindowTitle(windowTitle);
+
+        JFrame mainWindow = new JFrame(this.getWindowTitle());
+        mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainWindow.setSize(this.getWindowWidth(), this.getWindowHeight());
+        mainWindow.setVisible(true);
+    }
+
+    //TODO
+}
